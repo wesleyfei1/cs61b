6eca287697939b859a4180d1461fcba635a2ef5f
wesleyfei224@gmail.com <wesleyfei224@gmail.com>
2025-09-24 17:50:11 +0800

    finish hw6 version2

--- hw6/Boggle.java
+++ hw6/Boggle.java
@@ -5,7 +5,60 @@ import edu.princeton.cs.algs4.Stack;
 
 
 public class Boggle {
-    
+    public class TrieSet {
+        private static final int R=30;
+        public class Node{
+            boolean exists;
+            Node[] links;
+            public Node(){
+                links=new Node[R];
+                exists=false;
+            }
+        }
+        public TrieSet(String[] dict){
+            for(String str:dict){
+                put(str);
+            }
+        }
+        private Node root =new Node();
+        public Node getRoot(){
+            return root;
+        }
+        public void put(String key){
+            put(root,key,0);
+        }
+        private int transfer(char c){
+            if(c=='\'') return 0;
+            else if(c>='a'&& c<='z') return (c-'a'+1);
+            else return ('z'-'a'+2);
+        }
+        private Node put(Node x, String key, int d){
+            if(x==null){
+                x=new Node();
+            }
+            if(d==key.length()){
+                x.exists=true;
+                return x;
+            }
+            char c = key.charAt(d);
+            int idx = transfer(c);
+            x.links[idx]=put(x.links[idx],key,d+1);
+            return x;
+        }
+        public int find(String key){
+            Node x = root;
+            for (int i = 0; i < key.length(); i++) {
+                int idx = transfer(key.charAt(i));
+                if (x.links[idx] == null) {
+                    return -1;
+                }
+                x = x.links[idx];
+            }
+            if(x.exists==true) return 1;
+            else return 0;
+        }
+        //1.首先可以从上一个父母的地方开始继续，这样可以节省一点点的时间
+    }
     // File path of dictionary file
     public String dictPath = "words.txt";
     public char[][] board;
@@ -150,7 +203,7 @@ public class Boggle {
         });
         return result;
     }
-    public static void main(String[] args){
+    /**public static void main(String[] args){
         long start = System.nanoTime();
         Boggle b=new Boggle();
         String boardFilePath="exampleBoard.txt";
@@ -161,5 +214,5 @@ public class Boggle {
         long end = System.nanoTime();
         long elapsed = end - start; // 纳秒
         System.out.println("耗时: " + (elapsed / 1_000_000.0) + " ms");
-    }
+    }*/
 }
